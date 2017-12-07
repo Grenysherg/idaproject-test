646319b060ef13ce333fc70ce22fd329f6e681fc
Grenysherg <grenysherg@gmail.com>
2017-12-07 22:54:56 +0300

    Fix placeholder in IE9

--- app/blocks/card/card.js
+++ app/blocks/card/card.js
@@ -1,12 +1,14 @@
 import $ from 'jquery';
 import 'parsleyjs';
 import 'jquery-mask-plugin';
+import 'jquery-placeholder';
 
 $(() => {
 	const patternNumber = /[0-9]/;
 	const patterLetter = /[A-Za-z]/;
 
 	const cardForm = $('.card__form');
+	const ownerInput = cardForm.find('.card__item--owner').find('.input-field__input');
 
 	cardForm.parsley({
 		trigger: 'change',
@@ -17,9 +19,12 @@ $(() => {
 	cardForm.find('.card__form-section-item--number').find('.input-field__input').mask('0000', {translation: {
 		0: {pattern: patternNumber}
 	}});
-	cardForm.find('.card__item--owner').find('.input-field__input').mask('Z', {translation: {
+
+	ownerInput.placeholder();
+	ownerInput.mask('Z', {translation: {
 		Z: {pattern: patterLetter, recursive: true}
 	}});
+
 	cardForm.find('.card__item--code').find('.input-field__input').mask('000', {translation: {
 		0: {pattern: patternNumber}
 	}});
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,2 @@
-import $ from 'jquery';
-import placeholderPolyfill from 'input-placeholder-polyfill';
-
-$(() => {
-	placeholderPolyfill();
-});
-
 import '../blocks/select-field/select-field';
 import '../blocks/card/card';
