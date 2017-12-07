e9d5fedd7c64d581d7deb2b5c619a6a36985412f
Grenysherg <grenysherg@gmail.com>
2017-12-08 01:15:08 +0300

    Change public branch name

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -2,5 +2,5 @@ import gulp from 'gulp';
 import ghpages from 'gulp-gh-pages';
 
 gulp.task('deploy', () => (
-	gulp.src(['dist/**/*', '!dist/robots.txt']).pipe(ghpages({branch: 'dist'}))
+	gulp.src(['dist/**/*', '!dist/robots.txt']).pipe(ghpages({branch: 'gh-pages'}))
 ));
